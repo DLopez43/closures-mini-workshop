d74c0649bd56ab86b51ffa5b9efd42e1acb66c56
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-09-10 10:46:00 -0400

    saveOutput with Elijah

--- problems/saveOutput.js
+++ problems/saveOutput.js
@@ -18,8 +18,31 @@ multBy2AndSave(10);       // 20
 multBy2AndSave("showAll") // {5: 10, 10: 20}
 */
 
+/*
+  define success  -->  create an obj cache ; return the cache
+  key phrases
+    --> input for outer function --> takes in a function (func) and a string
+    --> output for outer function -->  return a new function
+    --> output for inner function 
+      -->  create an obj cache  OR  return the cache (string is magicWord)
+  match to actions
+    --> create a conditional to search for magic word
+    --> if no magic word, add to the cache (Key: string, Value: function acting on the string)
+  strategy (optional)
+*/
+
+// LMAOOOOOO --> too much chatting - LOL does
+// IT WORKSSSS !!! YAY 
 function saveOutput(func, magicWord) {
   // your code here
+  const cache = {};
+
+  return function (input) {
+    if (input === magicWord) return cache;
+    
+    cache[input] = func(input);
+    return cache[input]
+  }
 }
 
 /*
@@ -37,7 +60,7 @@ function runExamples() {
   console.log(multBy2AndSave(10));       // -> 20
 
   // TODO: Add ONE additional example call of your own, then unlock:
-  // e.g., console.log(multBy2AndSave(7));  // -> 14
+  console.log(multBy2AndSave(7));  // -> 14
   console.log(multBy2AndSave("showAll")); // -> {5: 10, 10: 20 /* , 7: 14 if you added it */}
 }
-// runExamples();
+runExamples();
